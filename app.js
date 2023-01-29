1f935e921e7f926fdd37353a5000b0c4c84795ff
realsarius <54468228+realsarius@users.noreply.github.com>
2023-01-29 19:02:41 +0300

    created table to display the books
    + stylized the table

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}.`;
-  };
 }
+
+Book.prototype.printInfo = function () {
+  return `${this.title}, ${this.author}, ${this.pages}, ${this.read}.`;
+};
 
 const book1 = new Book(
   'The Hobbit',
@@ -14,5 +17,32 @@ const book1 = new Book(
   '295 pages',
   'not read yet'
 );
+const book2 = new Book(
+  'Harry Potter',
+  'J.R.R. Tolkien',
+  '295 pages',
+  'not read yet'
+);
 
-console.log(book1.info());
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+const tbody = document.getElementById('tbody');
+
+function displayBooks() {
+  myLibrary.forEach((item, index) => {
+    const tr = document.createElement('tr');
+    Object.keys(item).forEach((k, i) => {
+      const td = document.createElement('td');
+
+      td.textContent = item[k];
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+}
+
+displayBooks();
