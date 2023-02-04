9988b6613376881ead3db71299789180ac83f389
realsarius <54468228+realsarius@users.noreply.github.com>
2023-02-04 20:46:11 +0300

    switched the book from function to class

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(_title, _author, _pages, _read, _UUID) {
+    this.title = _title;
+    this.author = _author;
+    this.pages = _pages;
+    this.read = _read;
+    this.uuid = _UUID;
+  }
 
-Book.prototype.printInfo = function () {
-  return `${this.title}, ${this.author}, ${this.pages}, ${this.read}.`;
-};
+  printInfo() {
+    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}, ${this.uuid}.`;
+  }
+}
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -25,12 +28,13 @@ function addBookToLibrary(book) {
   for (let key in book) {
     if (book.hasOwnProperty(key)) {
       const td = document.createElement('td');
-      if (key === 'title') {
-        tr.dataset.title = book[key];
-        deleteBtn.dataset.row = myLibrary.length;
+      if (key === 'uuid') {
+        tr.dataset.uuid = book[book.uuid];
+        deleteBtn.dataset.bookId = book.uuid;
+      } else {
+        td.textContent = book[key];
+        tr.appendChild(td);
       }
-      td.textContent = book[key];
-      tr.appendChild(td);
     }
     tr.appendChild(readBtn);
     tr.appendChild(deleteBtn);
@@ -45,31 +49,43 @@ newBook.addEventListener('click', (e) => {
 });
 
 const table = document.querySelector('#table');
-// function handleClick(e) {
-//   console.log('asddsa');
-//   table.deleteRow(e.target.getAttribute('data-row'));
-// }
-// const buttons = () => {
-//   const deleteButtons = document.querySelectorAll('#deleteBtn');
-//   deleteButtons.forEach((btn) => {
-//     btn.addEventListener('click', handleClick, { once: true });
-//   });
-// };
+
+function handleClick(e) {
+  myLibrary.find((o, i) => {
+    // console.log(`o: ${o.uuid}`);
+    // console.log(`e: ${e.target.getAttribute('data-book-id')}`);
+    if (o.uuid === e.target.getAttribute('data-book-id')) {
+      console.log('---------------------------');
+      console.log(`o: ${o.uuid}`);
+      console.log(`e: ${e.target.getAttribute('data-book-id')}`);
+      table.deleteRow(o.uuid);
+      return null;
+    }
+  });
+}
+
 const buttons = () => {
-  document.querySelectorAll('#deleteBtn').forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      table.deleteRow(btn.getAttribute('data-row'));
-    });
+  const deleteButtons = document.querySelectorAll('#deleteBtn');
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener('click', handleClick, { once: true });
   });
 };
+// const buttons = () => {
+//   document.querySelectorAll('#deleteBtn').forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//       table.deleteRow(btn.getAttribute('data-row'));
+//     });
+//   });
+// };
 
 const submitBtn = document.querySelector('#submitBtn');
 submitBtn.addEventListener('click', (e) => {
+  const UUID = Math.random().toString(16).slice(2);
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const page = document.querySelector('#page').value;
   const read = document.querySelector('#read').value;
-  const book = new Book(title, author, page, read);
+  const book = new Book(title, author, page, read, UUID);
 
   addBookToLibrary(book);
   buttons();
